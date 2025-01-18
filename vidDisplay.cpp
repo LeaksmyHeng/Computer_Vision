0c81227ffcf6f455e59e2fccabee4a29babd98ad
LeaksmyHeng <hengleaksmy1998@gmail.com>
2025-01-17 21:32:41 -0500

    add part 4
    if user press g then convert to greyscale

--- vidDisplay.cpp
+++ vidDisplay.cpp
@@ -11,15 +11,20 @@
 using namespace cv;
 using namespace std;
 
+
 int main(int argc, char *argv[]) {
     /**
      * This function open up the video camera and display it.
      * If users press 'q' => quite the program
      * If users press 's' => save an image to a file
+     * If users press 'g' => change the video to greyscale instead of color
+     * If users press 'c' => change the video to color again
      * Reference code:
      * https://learnopencv.com/read-write-and-display-a-video-using-opencv-cpp-python/
      * 
      */
+
+    // open video from webcamp
     cv::VideoCapture *capdev;
 
     // open the video device
@@ -36,6 +41,9 @@ int main(int argc, char *argv[]) {
 
     cv::namedWindow("Video", 1); // identifies a window
     cv::Mat frame;
+    cv::Mat grayFrame;
+
+    bool isGreyScaleVideo = false;
 
     while (true) {
         // get a new frame from the camera, treat as a stream
@@ -43,7 +51,16 @@ int main(int argc, char *argv[]) {
         if( frame.empty() ) {
             printf("frame is empty\n");
             break;
-        }                
+        }
+
+        if (isGreyScaleVideo == true) {
+            // convert color to greyscale through (COLOR_BGR2GRAY)
+            cv::cvtColor(frame, grayFrame, cv::COLOR_BGR2GRAY);
+            // Convert back to BGR for consistent display
+            cv::cvtColor(grayFrame, frame, cv::COLOR_GRAY2BGR);
+        }
+
+        // Display the current frame         
         cv::imshow("Video", frame);
 
         // see if there is a waiting keystroke
@@ -59,6 +76,13 @@ int main(int argc, char *argv[]) {
         }
         else if (key == 'g') {
             printf("Updating the video to grey scale instead of color.\n");
+            // Close the color window
+            isGreyScaleVideo = true;
+        }
+        else if (key == 'c') {
+            printf("Updating the video to back to color.\n");
+            // Close the color window
+            isGreyScaleVideo = false;
         }
     }
 
